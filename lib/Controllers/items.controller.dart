3a94280a93d9fb3996bc53f57afafada4272b58c
aqilgits <aqil.dev@gmail.com>
2024-02-18 02:32:11 +0800

    Update item.controller
    remove extra line

--- lib/Controllers/items.controller.dart
+++ lib/Controllers/items.controller.dart
@@ -2,6 +2,5 @@ double calcDiscount(int price, dynamic discount) {
   double total;
   total = discount / 100 * price;
   total = price - total;
-
   return double.parse(total.toStringAsFixed(2));
 }
